93b486023c09c3f582fd3d954f2829de32381d2d
mernstackproject <rammohangupta1769@gmail.com>
2025-08-16 18:46:03 +0530

    style:apply style in div

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div style={{ textAlign: 'center', marginTop: '50px' , backgroundColor: '#f0f0f0', padding: '20px', borderRadius: '10px'}}>
        <h1>John Do This is my first Ci Cd Example in Vite React</h1>
        </div>
     </>
