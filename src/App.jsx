827768b110f1978f6797df9065d8c8def67ae88f
mernstackproject <rammohangupta1769@gmail.com>
2025-08-17 11:25:29 +0530

    feat:add font size

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ function App() {
 
   return (
     <>
-      <div style={{ textAlign: 'center', marginTop: '50px' , backgroundColor: '#a02828ff', padding: '20px', borderRadius: '10px', color:"black"}}>
+      <div style={{ textAlign: 'center', marginTop: '50px' , backgroundColor: '#a02828ff', padding: '20px', borderRadius: '10px', color:"black", fontSize:"15px"}}>
        <h1>This is my first CI/CD Example in Vite React</h1>
+
        </div>
     </>
   )
