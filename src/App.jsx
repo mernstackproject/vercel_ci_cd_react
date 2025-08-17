017f59bd08c79536f56e1070c48d849d4e6baa28
mernstackproject <rammohangupta1769@gmail.com>
2025-08-17 11:32:49 +0530

    feat:add border

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <div style={{ textAlign: 'center', marginTop: '50px' , backgroundColor: '#a02828ff', padding: '20px', borderRadius: '10px', color:"black", fontSize:"15px"}}>
+      <div style={{ textAlign: 'center', marginTop: '50px' , backgroundColor: '#a02828ff', padding: '20px', borderRadius: '10px', color:"black", fontSize:"15px", border:"10px solid black"}}>
        <h1>This is my first CI/CD Example in Vite React</h1>
 
        </div>
