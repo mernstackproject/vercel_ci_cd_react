a5d8bab4633a30e4d7d6ede822b2f18d87f30366
mernstackproject <rammohangupta1769@gmail.com>
2025-08-17 11:11:34 +0530

    feat:add new h1 tag

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div style={{ textAlign: 'center', marginTop: '50px' , backgroundColor: '#f0f0f0', padding: '20px', borderRadius: '10px'}}>
-       <h1>John Do This is my first Ci Cd Example in Vite React</h1>
+       <h1>This is my first CI/CD Example in Vite React</h1>
        </div>
     </>
   )
